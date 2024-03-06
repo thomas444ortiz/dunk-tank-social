50cc04c095a4ef408474d74886a3025450ea9000
thomas444ortiz <thomasortiz444@gmail.com>
2024-03-06 17:59:00 -0500

    write functionality to check session

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -4,15 +4,37 @@ const sessionController = {};
 
 sessionController.createSession = (req, res, next) => {
     try {
-        models.Session.create({cookieId: `${res.locals.ssid}`})
-        .then((data)=>{
-            res.locals.session = data;
-            console.log(res.locals.session);
-            return next();
-        })
+        // check the db for an already existing session 
+        models.Session.findOne({cookieId: `${res.locals.ssid}`})
+        .then((data) =>{
+            // if the data is not null, then there is already a session so we can just move on
+            if(data !== null) return next();
+            // if no existing session, create one
+            models.Session.create({cookieId: `${res.locals.ssid}`})
+            .then((data)=>{
+                return next();
+            });
+        });
     } catch {
         return next('Error creating session');
     }
 }
+
+sessionController.verifySession = (req, res, next) => {
+    try {
+        console.log('verify session');
+        models.Session.findOne({cookieId: `${req.cookies.ssid}`})
+        .then((data) =>{
+            // if the data is not null, then there is a valid session and we can move on
+            if(data !== null) return next();
+            // but if the data is null, there is no session so we must redirect to the /login page
+            else return res.redirect('/login');
+        });
+        return next(); 
+    } catch {
+        return next('Failure to verify session')
+    }
+}
+
 
 module.exports = sessionController;
