189733f9c5b3a9cb0bc989fbd6ccffb409c559bc
thomas444ortiz <thomasortiz444@gmail.com>
2024-03-06 18:19:13 -0500

    create protected routes and store in state

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Routes, Route} from 'react-router-dom'
-import Navbar from './containers/Navbar'
 import SignupPage from './pages/SignupPage'
 import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
 import ProfilePage from './pages/ProfilePage'
+import PrivateRoutes from './PrivateRoutes'
 import './styles.css'
 
 export default function App() {
@@ -12,9 +12,11 @@ export default function App() {
     <div className="app">
       <Routes className="app">
         <Route  path="/" element={<LoginPage/>}/>
-        <Route  path="/home" element={<HomePage/>}/>
         <Route  path="/signup" element={<SignupPage/>}/>
-        <Route  path="/profile" element={<ProfilePage/>}/>
+        <Route element={<PrivateRoutes />}>
+          <Route  path="/home" element={<HomePage/>}/>
+          <Route  path="/profile" element={<ProfilePage/>}/>
+        </Route>
       </Routes>
     </div>
   );
